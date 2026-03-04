5c3fe1a8a156593c87bde1719d61628c8c2c77d3
Toksvig <htkdavid@gmail.com>
2026-03-04 21:43:44 +0100

    Start på programmet

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+# Det nødvendige biblotek bliver importeret her.
+import numpy as np
 
+# Det neurale netværk
+
+class NN:
+    def __init__(self,n_feature,n_classes,n_hidden):
+        self.d=n_feature
+        self.n=n_classes
+        self.h=n_hidden
+
+
+
+
+def main():
+    print("Test")
+
+
+
+
+# Programmet kan kun køres som script, og kan ikke importeres i andre scripts.
+if __name__ == "__main__":
+    main()
