4724f12a33a671d022a1ff99bc4d3b1916bbe4dc
Toksvig <htkdavid@gmail.com>
2026-03-05 20:34:02 +0100

    lille opdatering

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-# Det nødvendige biblotek bliver importeret her.
-import numpy as np
+# De nødvendige bibloteker bliver importeret her.
+import numpy as np # Til matematik
+import sys # Systemet
+import matplotlib # 
 
 # Det neurale netværk
 
-class NN:
-    def __init__(self,n_feature,n_classes,n_hidden):
-        self.d=n_feature
-        self.n=n_classes
-        self.h=n_hidden
+class tmp:
+    def __init__(self, inputs, weights, bias):
+        self.i = inputs
+        self.w = weights
+        self.b = bias
+
+
+    def NCalc(self):
+        output = self.i[0]*self.w[0]+self.i[1]*self.w[1]+self.i[2]*self.w[2]+ self.b
+        self.o = output
 
 
 
 
 def main():
-    print("Test")
+    # Versioner printes
+    print("Python version:", sys.version)
+    print("numpy version:", np.__version__)
+    print("Matplotlib version:", matplotlib.__version__)
+    inputs = [1.2,5.1,2.1]
+    weights = [3.1,2.1,8.7]
+    bias = 3
+    neuron = tmp(inputs,weights,bias)
+    neuron.NCalc()
+    print(neuron.o)
 
 
 
